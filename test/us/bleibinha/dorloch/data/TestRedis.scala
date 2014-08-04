0d438cccdb8e22fb9a6c11e85c11f058839e9c4e
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2014-08-04 13:59:22 +0200

    Make tests more robust

--- test/us/bleibinha/dorloch/data/TestRedis.scala
+++ test/us/bleibinha/dorloch/data/TestRedis.scala
@@ -1,7 +1,14 @@
 package us.bleibinha.dorloch.data
 
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 class TestRedis extends Redis {
   override protected val applicationString = "dorloch:test"
   val testApplicationString = applicationString
   val redisClient = client
+
+  //remove all previous test keys
+  Await.result(client.keys(s"$applicationString*") map client.del, 2 seconds)
 }
